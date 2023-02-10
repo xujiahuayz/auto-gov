724e7a5a119dedb956efa2f5cf1590a564423115
yebof <fengyebo@gmail.com>
2023-02-09 23:57:24 -0800

    update setup

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ setup(
         "tqdm==4.64.1",
         "torch==1.13.1",
         "gym==0.26.2",
+        "opencv-python==4.7.0.68",
+        "matplotlib==3.6.3",
+        "box2d-py==2.3.5",
+        "pygame==2.1.0",
+        "swig==4.1.1",
     ],
     extras_require={"dev": ["pylint", "black", "pytest"]},
 )
