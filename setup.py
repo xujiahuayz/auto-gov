a42dd2563b1a0d3ef54396615f1a51bf27fcc5ba
yebof <fengyebo@gmail.com>
2023-02-09 23:45:01 -0800

    update the env

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name="market_env",
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "pandas",
-        "tqdm",
+        "numpy==1.24.2",
+        "pandas==1.5.3",
+        "tqdm==4.64.1",
+        "torch==1.13.1",
+        "gym==0.26.2",
     ],
     extras_require={"dev": ["pylint", "black", "pytest"]},
 )
